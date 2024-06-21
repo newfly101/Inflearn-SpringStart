726738d471f1d115f222f0ff854799f60a850e2b
Jae-Hong-Kim <newfly101@naver.com>
2024-06-21 19:42:47 +0900

    first init

---
+++ src/test/java/com/daybreak/inflearnspring/InflearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daybreak.inflearnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
