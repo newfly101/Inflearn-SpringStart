3eac5ddfb9101a5823e501570f240c5864d9d839
Jae-Hong-Kim <newfly101@naver.com>
2024-06-23 06:18:37 +0900

    위랑 같은 구조로 작성도 가능함 (ctrl + t) 리팩토링 관련 (ctrl alt m)으로 진행하면 됨

--- src/main/java/com/daybreak/inflearnspring/service/MemberService.java
+++ src/main/java/com/daybreak/inflearnspring/service/MemberService.java
@@ -10,13 +10,20 @@ public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    /** 회원가입 **/
+    /**
+     * 회원가입
+     **/
     public long join(Member member) {
         // 같은 이름이 있는 중복 회원x
-        Optional<Member> result = memberRepository.findByName(member.getName()); // Optional을 꺼낼때 다음과 같이 진행
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+//        Optional<Member> result = memberRepository.findByName(member.getName()); // Optional을 꺼낼때 다음과 같이 진행
+//        result.ifPresent(m -> {
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
         memberRepository.save(member);
         return member.getId();
     }
