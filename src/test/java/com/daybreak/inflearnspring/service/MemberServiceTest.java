b62b6be7d1ccfee9baa6a125708745c9783cd434
Jae-Hong-Kim <newfly101@naver.com>
2024-06-23 06:23:57 +0900

    [Test] 테스트 코드 주석 추가

--- src/test/java/com/daybreak/inflearnspring/service/MemberServiceTest.java
+++ src/test/java/com/daybreak/inflearnspring/service/MemberServiceTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
+    // 테스트코드는 한글로도 많이 적음
+    // 빌드될때는 실제 코드에 포함되지 않음
 
     @Test
-    void join() {
+    void 회원가입() {
+
     }
 
     @Test
