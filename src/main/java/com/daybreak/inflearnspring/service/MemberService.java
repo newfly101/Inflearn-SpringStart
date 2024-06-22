491e4cd3626cf9d47cce784c061debbc160b470b
Jae-Hong-Kim <newfly101@naver.com>
2024-06-23 06:40:45 +0900

    [Test] 외부에서 repository를 넣어주도록 변경

--- src/main/java/com/daybreak/inflearnspring/service/MemberService.java
+++ src/main/java/com/daybreak/inflearnspring/service/MemberService.java
@@ -9,7 +9,13 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    // 외부에서 넣어주도록 변경함
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     /**
      * 회원가입
