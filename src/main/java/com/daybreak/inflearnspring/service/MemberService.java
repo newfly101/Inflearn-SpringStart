00a013c26900a5facb7885f78afcd4471495ca02
Jae-Hong-Kim <newfly101@naver.com>
2024-06-23 06:49:28 +0900

    annotation을 사용하지 않으면 인식하지 못한다.

--- src/main/java/com/daybreak/inflearnspring/service/MemberService.java
+++ src/main/java/com/daybreak/inflearnspring/service/MemberService.java
@@ -2,6 +2,7 @@ package com.daybreak.inflearnspring.service;
 
 import com.daybreak.inflearnspring.model.Member;
 import com.daybreak.inflearnspring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     // 외부에서 넣어주도록 변경함
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
