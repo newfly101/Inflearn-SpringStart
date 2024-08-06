c018fb578e1345a6f965c23edd844d8420b8ab37
Jae-Hong-Kim <newfly101@naver.com>
2024-08-06 22:57:50 +0900

    @Bean을 등록하는 방법 (annotation이 없어도 등록하는 경우)

---
+++ src/main/java/com/daybreak/inflearnspring/config/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.daybreak.inflearnspring.config;
+
+import com.daybreak.inflearnspring.repository.MemberRepository;
+import com.daybreak.inflearnspring.repository.MemoryMemberRepository;
+import com.daybreak.inflearnspring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
