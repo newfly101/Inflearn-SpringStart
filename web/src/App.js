c210d7ba412b5afe8cb099ff249a6b570908e405
Jae-Hong-Kim <newfly101@naver.com>
2024-08-07 05:04:40 +0900

    [test] 없어도 동작하길래 삭제

--- web/src/App.js
+++ web/src/App.js
@@ -2,22 +2,8 @@ import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Home from "./view/Home";
 import Member from "./view/Member";
 import MemberList from "./view/MemberList";
-import {useEffect} from "react";
-import axios from "axios";
 
 function App() {
-    useEffect(() => {
-        const checkCORS = async () => {
-            await axios.options("/", {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "*",
-                    "Access-Control-Allow-Method": "GET, DELETE, OPTIONS, POST"
-                }
-            });
-        }
-    }, []);
   return (
       <Router>
           <Routes>
